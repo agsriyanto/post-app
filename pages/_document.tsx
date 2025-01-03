10b9ba2c526db67cb9de0fccb7e71aa25bb44c9f
unknown <agsriyanto2@gmail.com>
2025-01-03 21:34:00 +0700

    init project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import type { DocumentContext } from 'next/document';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
 
-export default function Document() {
+const MyDocument = () => {
   return (
     <Html lang="en">
       <Head />
@@ -9,5 +11,34 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+MyDocument.getInitialProps = async (ctx: DocumentContext): Promise<any> => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      ),
+    });
+
+  const initialProps = await ctx.defaultGetInitialProps(ctx);
+
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
+  };
+};
+
+export default MyDocument;
